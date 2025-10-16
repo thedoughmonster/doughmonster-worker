cc7def0886e447767db53e98aa9a5bd6467520aa
thedoughmonster <thedoughmonster@gmail.com>
2025-10-16 09:18:33 -0400

    feat: reshape items-expanded orders

---
+++ src/types/toast-menus.d.ts
@@ -0,0 +1,11 @@
+export interface ToastMenusDocument {
+  [key: string]: any;
+}
+
+export interface ToastMenuItem {
+  [key: string]: any;
+}
+
+export interface ToastModifierOption {
+  [key: string]: any;
+}
---
+++ src/types/toast-orders.d.ts
@@ -0,0 +1,11 @@
+export interface ToastOrder {
+  [key: string]: any;
+}
+
+export interface ToastCheck {
+  [key: string]: any;
+}
+
+export interface ToastSelection {
+  [key: string]: any;
+}
